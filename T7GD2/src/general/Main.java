cf3dfee89ff82c8e5f0bace645b6f4af4850f413
gteset <gteset@free.fr>
2017-10-21 17:20:14 +0200

    Init menu principal

---
+++ T7GD2/src/general/Main.java
@@ -0,0 +1,42 @@
+package general;
+
+import java.io.File;
+
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.StateBasedGame;
+
+import menus.MainMenu;
+import menus.WelcomeMenu;
+
+public class Main extends StateBasedGame{
+	
+	public static int longueur=1280;
+	public static int hauteur=720;
+	
+	public static void main(String[] args) throws SlickException {
+		System.setProperty("org.lwjgl.librarypath", new File("natives").getAbsolutePath());
+		AppGameContainer app = new AppGameContainer(new Main(),longueur, hauteur, false);
+		app.setTargetFrameRate(60);
+		app.setVSync(true);
+		app.setShowFPS(true);
+		app.start();
+	}
+	
+
+	public Main() {
+		super("R2-T7G-D2");
+	}
+
+
+
+	@Override
+	public void initStatesList(GameContainer container) throws SlickException {
+		addState(new WelcomeMenu());
+		addState(new MainMenu());
+
+		
+		this.enterState(WelcomeMenu.ID);
+	}
+}
