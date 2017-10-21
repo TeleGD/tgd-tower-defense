10ed28e549ddc6af7687aa7c6f0c8fef9352e824
jeromegauzins <jeromegauzinsdev@gmail.com>
2017-10-21 16:47:24 +0200

    create package

--- T7GD2/src/Game.java
+++ T7GD2/src/Game.java
@@ -1,14 +1,17 @@
+package snake;
+
 import org.newdawn.slick.AppGameContainer;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.state.StateBasedGame;
 
+import java.io.File;
 
 
 public class Game extends StateBasedGame {
 
 	public static void main(String[] args) throws SlickException {
-		//System.setProperty("org.lwjgl.librarypath", new File("natives").getAbsolutePath());
+        System.setProperty("org.lwjgl.librarypath", new File("natives").getAbsolutePath());
 		AppGameContainer app = new AppGameContainer(new Game(),1280,720,false);
 		app.setTargetFrameRate(60);
 		app.setVSync(true);
---
+++ T7GD2/src/snake/World.java
@@ -0,0 +1,41 @@
+package snake;
+
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Input;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.BasicGameState;
+import org.newdawn.slick.state.StateBasedGame;
+
+public class World extends BasicGameState {
+
+
+	@Override
+	public void init(GameContainer container, StateBasedGame game) throws SlickException {
+	}
+
+	@Override
+	public void render(GameContainer container, StateBasedGame game, Graphics g) throws SlickException {
+	}
+
+	@Override
+	public void update(GameContainer container, StateBasedGame game, int delta) throws SlickException {
+	}
+
+	@Override
+	public int getID() {
+		// Aucune id�e
+		return 0;
+		
+	}
+	
+	public void keyReleased(int key, char c){
+	}
+	
+	public void keyPressed(int key, char c){
+
+	}
+	
+	
+
+}
