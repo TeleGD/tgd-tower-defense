d17d57927e0e9215fe1420b41f398add83535a57
jeromegauzins <jeromegauzinsdev@gmail.com>
2017-10-22 06:24:03 +0200

    lkzo

--- T7GD2/src/snake2/snake/World.java
+++ T7GD2/src/snake2/snake/World.java
@@ -379,7 +379,9 @@ public class World extends BasicGameState {
                     snake.body = new ArrayList<>();
 
                     for(int i=0;i<split.length/2;i++){
-                        snakes.get(i).body.add(new Point(Integer.parseInt(split[2*i]),Integer.parseInt(split[2*i+1])));
+                        if(2*i+2<split.length){
+                            snakes.get(i).body.add(new Point(Integer.parseInt(split[2*i+1]),Integer.parseInt(split[2*i+2])));
+                        }
                     }
 
                 }
